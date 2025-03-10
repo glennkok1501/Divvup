a0ed4c6f96859280fb4095cce5292cdd4cfba707
glennkok1501 <kok.glenn@yahoo.com.sg>
2025-03-10 21:06:17 +0800

    implemented user store

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
--- screens/home.tsx
+++ screens/home.tsx
@@ -1,27 +1,44 @@
-import ProfileView from "components/ProfileView";
-import TripsListView from "components/TripsListView";
-import { StyleSheet, View } from "react-native";
-import { Paragraph, Separator, Theme, YStack } from "tamagui";
+import ProfileView from 'components/ProfileView';
+import TripsListView from 'components/TripsListView';
+import { useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { useUserStore } from 'store/store';
+import { Text, Theme, YStack } from 'tamagui';
 
 const Home = () => {
-    return ( 
-        <View style={styles.container}>
-            <Theme name="light">
-                <YStack flex={1} gap="$5">
-                    <ProfileView />
-                    <TripsListView />
-                </YStack>
-            </Theme>
 
-            
-        </View>
-     );
-}
-export const styles = StyleSheet.create ({
+  const user = useUserStore((state) => state.user)
+  const setUser = useUserStore((state) => state.setUser)
+  
+  useEffect(() => {
+    if (user == null){
+      setUser({"id": 1, "username": "John Smith", "friendsCount": 123123})
+      console.log(user)
+    }
+  }, [user])
+
+  return (
+    <View style={styles.container}>
+      <Theme name="light">
+        {
+          (user == null) ? 
+          <View><Text>Not found</Text></View> 
+          :
+          <YStack flex={1} gap="$5">
+            <ProfileView user={user} />
+            <TripsListView />
+          </YStack>
+        }
+        
+      </Theme>
+    </View>
+  );
+};
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
-    paddingTop: 80
+    paddingTop: 80,
   },
 });
 export default Home;
