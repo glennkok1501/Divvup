013eaadae789726f0f2f05a0c5addd01a3e24222
glennkok1501 <kok.glenn@yahoo.com.sg>
2025-03-11 18:29:03 +0800

    changed statusbar color

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -2,6 +2,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import TabNavigator from './TabNavigator';
+import { StatusBar } from 'expo-status-bar';
 
 // import { BackButton } from '../components/BackButton';
 // import Details from '../screens/details';
@@ -30,10 +31,13 @@ export default function RootStack() {
     //   </Stack.Navigator>
     // </NavigationContainer>
     <NavigationContainer>
+      <StatusBar style="dark"/>
+      
       <Stack.Navigator>
         <Stack.Screen name="Home" component={TabNavigator} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
+
     
   );
 }
