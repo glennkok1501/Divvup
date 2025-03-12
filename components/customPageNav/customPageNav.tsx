36de45b472ebfcf2cf494fa9329dce60e4889950
glennkok1501 <kok.glenn@yahoo.com.sg>
2025-03-12 11:16:24 +0800

    implemented custom navigation tabs

---
+++ components/customPageNav/customPageNav.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+const CustomPageNav = ({components, headers}: {components: any, headers: any}) => {
+    
+    const [selected, setSelected] = useState(0)
+
+    const selectComponent = (s: string) => {
+        for (var i = 0; i < headers.length; i++) {
+            if (headers[i] == s) {
+                setSelected(i)
+            }
+        }
+    }
+
+    return ( 
+        <View style={styles.navigator}>
+            <View style={styles.tabsRow}>
+                {
+                    headers.map((header: string) => (
+                        <Pressable onPress={() => selectComponent(header)} style={[styles.tab, headers[selected] == header && styles.selected]} key={header}>
+                            <Text style={[styles.tabText, headers[selected] == header && styles.selected]}>
+                                {header}
+                            </Text>
+                        </Pressable>
+                    ))
+                }
+            </View>
+            <View style={styles.page}>
+                {components[selected]}
+            </View>
+        </View>
+     );
+}
+
+const styles = StyleSheet.create({
+    navigator: {
+        flex: 1,
+    },
+    tabsRow: {
+        flexDirection: "row",
+        justifyContent: "space-evenly"
+    },
+    tab: {
+        
+        padding: 10,
+        flexGrow: 1,
+        alignItems: "center",
+    },
+    selected: {
+        color: "white",
+        borderRadius: 25,
+        backgroundColor: "grey",
+    },
+    tabText: {
+        fontSize: 14,
+        color: "grey"
+    },
+    page: {
+        padding: 5,
+        marginTop: 10
+    }
+})
+ 
+export default CustomPageNav;
